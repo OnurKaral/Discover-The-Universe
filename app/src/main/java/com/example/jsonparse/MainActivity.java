fc913a8e463af3e7b2aafb34a7f6c5183c38a9e4
OnurKaral <santobixua@gmail.com>
2020-05-27 20:43:10 +0300

    HomePage *Calendar *Select Any Time to see APOD

--- app/src/main/java/com/example/jsonparse/MainActivity.java
+++ app/src/main/java/com/example/jsonparse/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.jsonparse;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,10 +18,14 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private TextView imagetitle;
@@ -26,16 +34,24 @@ public class MainActivity extends AppCompatActivity {
     private TextView imageinfo;
     private TextView imagedate;
     private BottomAppBar bottomAppBar;
+    private FloatingActionButton floatingActionButton;
 
+    private Calendar calendar;
+    private SimpleDateFormat dateFormat;
+    String urldate ;
+    private DatePickerDialog.OnDateSetListener dateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        calendar = Calendar.getInstance();
+        dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        urldate = dateFormat.format(calendar.getTime());
+
         bottomAppBar = findViewById(R.id.bar);
         setSupportActionBar(bottomAppBar);
-
         bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,20 +59,44 @@ public class MainActivity extends AppCompatActivity {
                 fragment.show(getSupportFragmentManager(),"TAG");
             }
         });
+            imagetitle = findViewById(R.id.titletv);
+            imageView = findViewById(R.id.image_view);
+            imageinfo = findViewById(R.id.infotv);
+            imagedate = findViewById(R.id.datetv);
 
-    imagetitle = findViewById(R.id.titletv);
-    imageView = findViewById(R.id.image_view);
-    imageinfo = findViewById(R.id.infotv);
-    imagedate = findViewById(R.id.datetv);
-
-    requestQueue = Volley.newRequestQueue(this);
+        requestQueue = Volley.newRequestQueue(this);
         jsonParse();
+
+        floatingActionButton = findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(MainActivity.this,android.R.style.Theme_Holo_Light_Dialog_MinWidth,dateSetListener,year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+            }
+        });
+
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month =month+1;
+                String date = year + "-" + month + "-" + dayOfMonth;
+                urldate =date;
+                jsonParse();
+            }
+        };
     }
 
     private void jsonParse() {
-      String url= "https://api.nasa.gov/planetary/apod?api_key=hhOItewgwlQmkaSH6xq7aZMpnLqCisxdUdomDfi3";
-     //   String url= "https://api.nasa.gov/planetary/apod?api_key=hhOItewgwlQmkaSH6xq7aZMpnLqCisxdUdomDfi3&date=2020-05-15";
-
+      //String url= "https://api.nasa.gov/planetary/apod?api_key=hhOItewgwlQmkaSH6xq7aZMpnLqCisxdUdomDfi3";
+        String url= "https://api.nasa.gov/planetary/apod?api_key=hhOItewgwlQmkaSH6xq7aZMpnLqCisxdUdomDfi3&date="+urldate;
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
@@ -87,7 +127,6 @@ public class MainActivity extends AppCompatActivity {
         });
         requestQueue.add(request);
     }
-
 }
 
 
