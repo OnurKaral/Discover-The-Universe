7fdff584ab6c9831466f7b176b2dbc1b96a95b67
OnurKaral <santobixua@gmail.com>
2020-06-23 20:53:04 +0300

    Homepage *Click for download image *Still working for share image *Working again on HD option

--- app/src/main/java/com/example/jsonparse/MainActivity.java
+++ app/src/main/java/com/example/jsonparse/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.jsonparse;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -32,6 +36,9 @@ import com.squareup.picasso.Picasso;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -51,7 +58,7 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
     private String URL;
     private String HDURL;
     private ProgressBar progressBar;
-
+    private Uri bmpUri = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -83,7 +90,7 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Uri uri = Uri.parse(HDURL); // missing 'http://' will cause crashed
+                Uri uri = Uri.parse(URL); // missing 'http://' will cause crashed
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
             }
@@ -119,14 +126,21 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
                     response.getString("explanation");
                     response.getString("date");
                     response.getString("media_type");
-                    response.getString("hdurl");
+                    /*
+                    if(response.getString("hdurl")!=null){
+                      response.getString("hdurl");
+
+                    }else{
+
+                    }
+                    String image_hd_url = response.getString("hdurl");  */
 
                     String image_date = response.getString("date");
                     String image_info = response.getString("explanation");
                     String imagename = response.getString("title");
                     String image_url = response.getString("url");
                     String image_type = response.getString("media_type");
-                    String image_hd_url = response.getString("hdurl");
+
 
                     imagedate.setText(image_date);
                     imageinfo.setText(image_info);
@@ -148,6 +162,7 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
                     videobutton.setVisibility(View.INVISIBLE);
 
                     if (image_type.equals("video")) {
+                        HDURL = null;
                         progressBar.setVisibility(View.INVISIBLE);
                         imageView.setVisibility(View.GONE);
                         videobutton.setVisibility(View.VISIBLE);
@@ -162,7 +177,7 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
 
                     }
                     URL = image_url;
-                    HDURL = image_hd_url;
+
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -179,25 +194,54 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
             }
         });
         requestQueue.add(request);
-
     }
 
+    public Uri getLocalBitmapUri(ImageView imageView) {
+        // Extract Bitmap from ImageView drawable
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable) {
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else {
+            return null;
+        }
+        // Store image to default external storage directory
+
+        try {
+            File file = new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS), "share_image_" + System.currentTimeMillis() + ".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
+    }
 
     //BottomAppbarNavigation
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.sendbutton:
-                Intent sendIntent = new Intent();
-                sendIntent.setAction(Intent.ACTION_SEND);
-                sendIntent.putExtra(Intent.EXTRA_TEXT, URL);
-                sendIntent.setType("text/plain");
-
-                Intent shareIntent = Intent.createChooser(sendIntent, null);
-                startActivity(shareIntent);
-                break;
             case R.id.downloadbutton:
+                Uri bmpUri = getLocalBitmapUri(imageView);
+                Snackbar snackbar = Snackbar.make(imagedate, "Image Downloaded.", Snackbar.LENGTH_LONG);
+                snackbar.setAnchorView(floatingActionButton);
+                snackbar.show();
                 break;
+            case R.id.sendbutton: /*
+
+                // Construct a ShareIntent with link to image
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                Uri screenshotUri = getLocalBitmapUri(imageView);
+                shareIntent.setType("image/*");
+                shareIntent.putExtra(Intent.EXTRA_STREAM, screenshotUri);
+                // Launch sharing dialog for image
+                startActivity(Intent.createChooser(shareIntent, "Share Image"));
+                break;
+                */
         }
         return true;
     }
