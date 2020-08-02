453acffb62b6ba056e64a8e9dc81e63b6c5b41b9
qwertyF <santobixua@gmail.com>
2020-08-02 21:50:15 +0300

    Onboarding Screen *Fix

--- app/src/main/java/com/example/DiscoverTheUniverse/SplashScreen.java
+++ app/src/main/java/com/example/DiscoverTheUniverse/SplashScreen.java
@@ -3,15 +3,27 @@ package com.example.DiscoverTheUniverse;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        textView = findViewById(R.id.tv_skip);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -21,5 +33,7 @@ public class SplashScreen extends AppCompatActivity {
 
             }
         }, 4200);
+
     }
+
 }
